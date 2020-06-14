eec16c50910b68a679d0c2002efa7efae091750c
kyngs_kwk <kyngskwk@gmail.com>
2020-06-15 04:03:56 +0900

    moviesjson plus cast

--- api.py
+++ api.py
@@ -4,6 +4,7 @@ import json
 
 
 results = []
+cast = []
 
 for i in range(1, 10):
     url = "https://api.themoviedb.org/3/movie/upcoming?api_key=d689b32a04ce5d8848100e823ed584d7&language=ko-KR&page=" + str(i)
@@ -12,14 +13,32 @@ for i in range(1, 10):
     for i in data:
         new = {}
         new['model'] = 'movies.movie'
-        new['id'] = i.pop('id')
+        movie_id = i.pop('id')
+        new['id'] = movie_id
+        
+        url = "https://api.themoviedb.org/3/movie/" + str(movie_id) + "/credits?api_key=d689b32a04ce5d8848100e823ed584d7"
+        cast_data = requests.get(url).json()["cast"]
+        cast_id = []
+        # print(cast_data)
+        cnt = 0
+        for c in cast_data:
+            cast_id.append(c["id"])
+            cnt += 1
+            if cnt == 5: break
 
         k = {}
         for key, value in i.items():
             if key in ['title', 'overview', 'vote_average', 'poster_path', 'like_users']:
                 k[key] = value
+            k['cast'] = cast_id
+
+        print(k)
         new['fields'] = k
         results.append(new)
+
+
+        # for data in results:
+        #     data["fileds"][]
 
 with open('movies.json', 'w', encoding="UTF-8") as make_file:
         json.dump(results, make_file, indent="\t") 
