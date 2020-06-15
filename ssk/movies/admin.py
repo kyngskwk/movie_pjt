ecc2d2e51a5ce02a8e5c7fdf4b20dffcb87c8b89
kyngs_kwk <kyngskwk@gmail.com>
2020-06-15 14:35:15 +0900

    cast.json

--- ssk/movies/admin.py
+++ ssk/movies/admin.py
@@ -1,10 +1,8 @@
 from django.contrib import admin
-from .models import Movie, Review, MovieComment, ReviewComment
+from .models import Movie, Review, MovieComment, ReviewComment, Cast
 
 admin.site.register(Movie)
 admin.site.register(Review)
 admin.site.register(MovieComment)
 admin.site.register(ReviewComment)
-
-
 # Register your models here.
--- ssk/movies/models.py
+++ ssk/movies/models.py
@@ -1,14 +1,30 @@
 from django.db import models
 from django.conf import settings
 
+
 # Create your models here.
+
+
+class Cast(models.Model):
+    character = models.CharField(max_length=100)
+    name = models.CharField(max_length=100)
+    title = models.CharField(max_length=200)
+    poster_path = models.CharField(max_length=200, null=True)
+    overview = models.TextField(blank=True)
+    gender = models.IntegerField()
+    profile_path = models.CharField(max_length=200, null=True)
+    imdb_id = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.name
+
 
 class Movie(models.Model):
     title = models.CharField(max_length=100)
     overview = models.TextField(blank=True)
     vote_average = models.IntegerField()
     poster_path = models.CharField(max_length=200, null=True)
-    like_users = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name='like_movies', blank=True)
+    cast = models.ManyToManyField(Cast, related_name='cast', blank=True)
 
     def __str__(self):
         return self.title
